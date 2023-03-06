0af4967369ff6b39b5cbd13b44968073d72f49bd
Sergey Murawsky <smrwsky@gmail.com>
2023-03-07 01:20:34 +0400

    fix: Fix config files

--- eslint-config-react.js
+++ eslint-config-react.js
@@ -1,51 +1,37 @@
-const eslintPluginJsxA11y = require('eslint-plugin-jsx-a11y');
-const eslintPluginReact = require('eslint-plugin-react');
-const eslintPluginReactHooks = require('eslint-plugin-react-hooks');
-const eslintPluginReactPerf = require('eslint-plugin-react-perf');
-module.exports = [
-  {
-    files: ['**/*.js', '**/*.jsx', ' **/*.tsx'],
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
+module.exports = {
+  plugins: ['jsx-a11y', 'react', 'react-hooks', 'react-perf'],
+  extends: [
+    'plugin:import/react',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-perf/all',
+  ],
+  rules: {
+    'import/no-default-export': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-sort-props': ['error', { callbacksLast: true }],
+    'react/no-array-index-key': 'off',
+    'react-perf/jsx-no-new-object-as-prop': [
+      'error',
+      {
+        nativeAllowList: ['style'],
       },
-    },
-    plugins: {
-      'jsx-a11y': eslintPluginJsxA11y,
-      'react': eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
-      'react-perf': eslintPluginReactPerf,
-    },
-    rules: {
-      ...eslintPluginJsxA11y.configs.recommended.rules,
-      ...eslintPluginReact.configs.recommended.rules,
-      ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactPerf.configs.all.rules,
-      'import/no-default-export': 'off',
-      'react/destructuring-assignment': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-props-no-spreading': 'off',
-      'react/jsx-sort-props': ['error', { callbacksLast: true }],
-      'react/no-array-index-key': 'off',
-      'react-perf/jsx-no-new-object-as-prop': [
-        'error',
-        {
-          nativeAllowList: ['style'],
-        },
-      ],
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    settings: {
-      'import/extensions': ['.js', '.mdx'],
+  overrides: [
+    {
+      files: ['**/*.jsx'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
     },
-  },
-];
+  ],
+};
--- eslint-config-typescript.js
+++ eslint-config-typescript.js
@@ -1,52 +1,51 @@
-const typescriptEslintPlugin = require('@typescript-eslint/eslint-plugin');
-const typescriptEslintParser = require('@typescript-eslint/parser');
-const eslintPluginImport = require('eslint-plugin-import');
-
-const eslintBase = require('./eslint-config.js');
-
-module.exports = [
-  ...eslintBase,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: typescriptEslintParser,
-    },
-    plugins: {
-      '@typescript-eslint': typescriptEslintPlugin,
-    },
-    rules: {
-      ...typescriptEslintPlugin.configs['eslint-recommended'].rules,
-      ...typescriptEslintPlugin.configs.recommended.rules,
-      ...typescriptEslintPlugin.configs['recommended-requiring-type-checking']
-        .rules,
-      ...eslintPluginImport.configs.typescript.rules,
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        { checksVoidReturn: false },
+module.exports = {
+  extends: ['./eslint-config'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        extraFileExtensions: ['.json'],
+        sourceType: 'module',
+        ecmaVersion: 6,
+        ecmaFeatures: {
+          modules: true,
+        },
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/typescript',
       ],
-      '@typescript-eslint/no-use-before-define': 'error',
-      '@typescript-eslint/quotes': 'error',
-      'no-underscore-dangle': 'error',
-      'quotes': 'off',
-      'react/prop-types': 'off',
-      'react/require-default-props': 'off',
-      'require-jsdoc': 'off',
-    },
-    settings: {
-      ...eslintPluginImport.configs.typescript.settings,
-      'import/extensions': ['.ts', '.tsx', '.js', '.jsx', '.mdx'],
-      'import/resolver': {
-        node: {
-          extensions: ['.ts', '.tsx', '.js', '.jsx', '.mdx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          { checksVoidReturn: false },
+        ],
+        '@typescript-eslint/no-use-before-define': 'error',
+        '@typescript-eslint/quotes': 'error',
+        'no-underscore-dangle': 'error',
+        'quotes': 'off',
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+        'require-jsdoc': 'off',
+      },
+      settings: {
+        'import/extensions': ['.ts', '.tsx', '.js', '.jsx', '.mdx'],
+        'import/resolver': {
+          node: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.mdx'],
+          },
         },
       },
     },
-  },
-  {
-    files: ['**/*.d.ts'],
-    rules: {
-      'import/no-default-export': 'off',
+    {
+      files: ['**/*.d.ts', '**/*.tsx'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
     },
-  },
-];
+  ],
+};
